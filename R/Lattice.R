18e90c75b164235d9005bab6384990eaf4c448e8
Fernando H Toledo <f.toledo@cgiar.org>
2026-03-30 14:08:14 -0600

    Refactor Lattice to deal with variations

--- R/Lattice.R
+++ R/Lattice.R
@@ -15,29 +15,129 @@
 Lattice <- setRefClass(
     Class = "Lattice",
     contains = "Breeder",
-    fields = c("report" = "list"),
+    fields = c("report" = "list", "layout" = "logical", "check" = "logical"),
     methods = list(
         initialize = function(d) {
+            
             if(inherits(d, "Breeder")) {
+                
                 .self$data <- d$data
                 .self$log <- d$log
+                .self$layout <- .self$check <- FALSE
+
+                if (!is.factor(.self$data$replication))
+                    .self$data$replication <- factor(.self$data$replication)
+                
+                if (!is.factor(.self$data$block))
+                    .self$data$block <- factor(.self$data$block)
+
+                if (!is.factor(.self$data$treat))
+                    .self$data$treat <- factor(.self$data$treat)
+
+                if (all(c("row", "column") %in% names(.self$data))) {
+
+                    if (all(!is.na(.self$data$row)) & all(!is.na(.self$data$column))) {
+
+                        .self$data$row <- factor(.self$data$row)
+                        .self$data$column <- factor(.self$data$column)
+                        .self$data <- .self$data[with(.self$data, order(row, column)), ]
+                        .self$layout <- TRUE
+                        
+                    } else {
+                        
+                        .self$layout <- FALSE
+                        
+                    }
+
+                }
+
+                if ("role" %in% names(.self$data)) {
+
+                    if (all(!is.na(.self$data$role))) {
+
+                        .self$data$role <- factor(.self$data$role)
+
+                        if (all(levels(.self$data$role) %in% c("check", "test"))) 
+                            .self$check <- TRUE
+                    
+                    } else {
+                        
+                        .self$check <- FALSE
+                        
+                    }
+
+                }
+                
                 .self$report <- .self$analyze()
+                
             }
+            
         },
         analyze = function() {
-            fit <- lme4::lmer(response ~ replication + treat + (1 | replication:block),
-                              data = .self$data)
-            ll   <- as.numeric(logLik(fit))
-            reml <- lme4::REMLcrit(fit)
-            sigma <- sigma(fit)
-            anova <- emmeans::joint_tests(fit, lmer.df = "kenward-roger")
-            vc <- lme4::VarCorr(fit)
-            means <- emmeans::emmeans(fit, specs = ~ treat, lmer.df = "kenward-roger")
+
+            asreml::asreml.options(maxit = 100, workspace = 3E8, pworkspace = 1E8, trace = FALSE)
+            
+            if (.self$layout & .self$check) {
+
+                fit <- asreml::asreml(response ~ role + at(role, "check"):treat,
+                                      random = ~ replication + replication:block + at(role, "test"):treat,
+                                      residual = ~ ar1(row):ar1(column),
+                                      data = .self$data)
+
+                checks <- subset(predict(fit, classify = "role:treat", levels = list(role = "check"))$pvals,
+                                 status == "Estimable", select = -status)
+            
+                tests <- subset(predict(fit, classify = "role:treat", levels = list(role = "test"))$pvals,
+                                !(treat %in% as.character(checks$treat)) & status == "Estimable", select = -status)
+
+            } else if (.self$layout & !(.self$check)) {
+
+                fit <- asreml::asreml(response ~ 1,
+                                      random = ~ replication + replication:block + treat,
+                                      residual = ~ ar1(row):ar1(column),
+                                      data = .self$data)
+
+                means <- subset(predict(fit, classify = "treat")$pvals,
+                                status == "Estimable", select = -status)
+                
+            } else if (!(.self$layout) & .self$check) {
+
+                fit <- asreml::asreml(response ~ role + at(role, "check"):treat,
+                                      random = ~ replication + replication:block + at(role, "test"):treat,
+                                      data = .self$data)
+
+                checks <- subset(predict(fit, classify = "role:treat", levels = list(role = "check"))$pvals,
+                                 status == "Estimable", select = -status)
+            
+                tests <- subset(predict(fit, classify = "role:treat", levels = list(role = "test"))$pvals,
+                                !(treat %in% as.character(checks$treat)) & status == "Estimable", select = -status)
+
+            } else if (!(.self$layout) & !(.self$check)) {
+
+                fit <- asreml::asreml(response ~ 1,
+                                      random = ~ replication + replication:block + treat,
+                                      data = .self$data)
+
+                means <- subset(predict(fit, classify = "treat")$pvals,
+                                status == "Estimable", select = -status)
+
+            }
+
+            ll <- summary(fit)$loglik
+            reml <- -2 * ll
+            sigma <- summary(fit)$sigma
+            anova <- asreml::wald(fit)
+            vc <- summary(fit)$varcomp
+
+            if (.self$check)
+                means <- rbind(checks, tests)
+                                                
             .self$appendLog(event = "report")
             .self$report <-  list("fit" = list("LogLik" = ll, "REML" = reml, "sigma" = sigma),
                                   "anova" = anova,
                                   "variance" = as.data.frame(vc),
                                   "means" = means)
+            
         },
         asList = function() {
             return(list(data = .self$data, report = .self$report, log = .self$log))
--- tests/testthat/test-Lattice.R
+++ tests/testthat/test-Lattice.R
@@ -12,6 +12,14 @@ test_that("Lattice concrete class test", {
     expect_true(is(l, "Lattice")) 
 })
 
-test_that("Lattice concrete class sigma check", {
-    expect_equal(round(l$report$fit$sigma, 4), 17.9712) ## see reference
+## test_that("Lattice concrete class sigma check", {
+##     expect_equal(round(l$report$fit$sigma, 4), 17.9712) ## see reference
+## })
+
+test_that("Lattice anova terms", {
+    expect_true(all(c("(Intercept)", "residual (MS)") %in% rownames(l$report$anova)))
+})
+
+test_that("Lattice variance components", {
+    expect_true(all(c("replication", "replication:block") %in% rownames(l$report$variance)))
 })
